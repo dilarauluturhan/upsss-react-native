94604873474467e2518c3cda2ea4f5b0a45320b2
dilarauluturhan <dilarauluturhan@outlook.com>
2024-12-02 13:33:14 +0300

    style:login-page-UI

--- App.tsx
+++ App.tsx
@@ -1,10 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  Pressable,
+} from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Image style={styles.logo} source={require("./assets/logo.png")} />
+
+      <Text style={styles.textStyle}>E-mail:</Text>
+      <TextInput
+        placeholder="Enter your e-mail"
+        style={styles.textInputStyle}
+        onChangeText={setMail}
+        value={mail}
+      />
+
+      <Text style={styles.textStyle}>Password:</Text>
+      <TextInput
+        placeholder="Enter your password"
+        style={styles.textInputStyle}
+        onChangeText={setPassword}
+        value={password}
+      />
+
+      <Pressable
+        onPress={() => console.log("onpress")}
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed ? "#E07B39" : "#F29F58",
+          },
+          styles.loginButton,
+        ]}
+      >
+        <Text style={styles.loginButtonText}>Login</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +55,36 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  logo: {
+    width: "60%",
+    height: "15%",
+  },
+  headerText: {
+    fontSize: 32,
+    color: "#F29F58",
+  },
+  textStyle: {},
+  textInputStyle: {
+    borderWidth: 1,
+    width: "70%",
+    height: 40,
+    borderRadius: 10,
+    marginVertical: 7,
+    textAlign: "left",
+  },
+  loginButton: {
+    width: "70%",
+    height: 35,
+    borderRadius: 7,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 10,
+  },
+  loginButtonText: {
+    color: "#FFF",
+    textAlign: "center",
+    fontSize: 15,
+    fontWeight: "bold",
   },
 });
