78b44f466528d16ed59f906cba3cb661b8104c6f
dilarauluturhan <dilarauluturhan@outlook.com>
2024-12-28 15:16:45 +0300

    feat:bottom-tabs

--- navigation/UserStack.tsx
+++ navigation/UserStack.tsx
@@ -1,15 +1,73 @@
 import HomepageScreen from "../app/HomepageScreen";
 import ProfileScreen from "../app/ProfileScreen";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ExploreScreen from "../app/ExploreScreen";
+import NotificationScreen from "../app/NotificationScreen";
+import CartScreen from "../app/CartScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const UserStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="HomepageScreen" component={HomepageScreen} />
-      <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
-    </Stack.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tab.Screen
+        name="HomepageScreen"
+        component={HomepageScreen}
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home-outline" size={22} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="ExploreScreen"
+        component={ExploreScreen}
+        options={{
+          title: "Explore",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="search-outline" size={22} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="NotificationScreen"
+        component={NotificationScreen}
+        options={{
+          title: "Notification",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="notifications-outline" size={22} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="CartScreen"
+        component={CartScreen}
+        options={{
+          title: "Cart",
+          tabBarBadge: 3,
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="cart-outline" size={22} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="ProfileScreen"
+        component={ProfileScreen}
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person-outline" size={22} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
