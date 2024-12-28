c1e1117640d8892e16ecf0c7200ea3aba4901dbb
dilarauluturhan <dilarauluturhan@outlook.com>
2024-12-28 13:51:19 +0300

    feat:constant-colors

---
+++ constant/Colors.ts
@@ -0,0 +1,9 @@
+export const Colors = {
+    primary: '#E07B39',
+    black: '#333',
+    gray: '#666',
+    lightGray: '#999',
+    white: '#fff',
+    background: '#F4F4F4',
+    highlight: '#0A5EB0',
+  }
